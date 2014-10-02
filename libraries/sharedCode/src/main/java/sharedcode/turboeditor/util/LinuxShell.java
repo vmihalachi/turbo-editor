a20f93f0f6fabd5366b701d3c3befbf9d95d4b52
DF1E <dnielfe@gmail.com>
2014-10-02 18:33:44 +0200

    remove old info from LinuxShell

--- libraries/sharedCode/src/main/java/sharedcode/turboeditor/util/LinuxShell.java
+++ libraries/sharedCode/src/main/java/sharedcode/turboeditor/util/LinuxShell.java
@@ -17,21 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- *   920 Text Editor is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   920 Text Editor is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with 920 Text Editor.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package sharedcode.turboeditor.util;
 
 import java.io.BufferedReader;
